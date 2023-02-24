45a127b0b48b26d1825f67e42f3529f9f28bf51b
Serhii Revva <besha2vox@gmail.com>
2023-02-24 21:20:16 +0200

    add middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const middleware = [thunk, logger];
 
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: [...getDefaultMiddleware(), ...middleware],
 });
 
 export { store };
